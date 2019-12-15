bcc68c3b03535cec2841cdd9968b05efcebc3c66
Sharis <sarezas@gmail.com>
2019-12-15 12:57:24 +0200

    temp-css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormBuilder, AbstractControl, FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'temp-css';
+  constructor(private fb: FormBuilder) {}
+  title = "temp-css";
+  isInvalid = false;
+
+  form = this.fb.group({
+    email: new FormControl("")
+  });
+
+  onSubmit() {
+    console.log(this.form.controls.email.value);
+    this.isInvalid = true;
+
+    setTimeout(() => {
+      this.isInvalid = false;
+    }, 5000);
+  }
 }
